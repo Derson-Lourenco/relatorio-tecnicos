05349936c1cdc89dc421c042c7dbe7ad2eb73bbd
Anderson Lourenço <68036013+Derson-Lourenco@users.noreply.github.com>
2024-08-28 21:27:51 -0300

    add inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-// App.js ou Routes.js
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { RelatorioProvider } from './context/Contexto';
 import SelecionarTecnico from './components/SelecionarTecnico';
 import SelecionarServico from './components/SelecionarServico';
 import Finalizar from './components/Finalizar';
 import RelatorioCompleto from './components/RelatorioCompleto';
+import Inicio from './components/Inicio'; // Importar o componente Inicio
 
 function App() {
   return (
     <RelatorioProvider>
       <Router>
         <Routes>
+          <Route path="/" element={<Inicio />} /> {/* Rota inicial */}
           <Route path="/selecionar-tecnico" element={<SelecionarTecnico />} />
           <Route path="/selecionar-servico" element={<SelecionarServico />} />
           <Route path="/finalizar" element={<Finalizar />} />
